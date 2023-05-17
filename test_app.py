67084bec663b6286637536f28316a0e9a8fa951a
Jordi <Jordi@hello.nl>
2023-05-17 13:52:11 +0200

    Feature: added City

--- test_app.py
+++ test_app.py
@@ -13,13 +13,13 @@ class TestApp(TestCase):
     def test_index_page(self):
         response = self.client.get(url_for('index'))
         self.assert200(response)
-        self.assertIn(b"Enter Your Name and Age", response.data)
+        self.assertIn(b"Enter Your Name, Age and City", response.data)
 
     def test_greet_page(self):
-        response = self.client.post(url_for('greet'), data={'name': 'John', 'age': 25}, follow_redirects=True)
+        response = self.client.post(url_for('greet'), data={'name': 'John', 'age': 25, 'city': 'Groningen'}, follow_redirects=True)
         self.assert200(response)
         self.assertIn(b"Hello, John!", response.data)
-        self.assertIn(b"You are 25 years old.", response.data)
+        self.assertIn(b"You are 25 years old and you live in Groningen.", response.data)
 
 if __name__ == '__main__':
     unittest.main()
