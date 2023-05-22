25884cb7a5cf9d15cb7be8d617bd3e2f8a2ac259
Jordi <Jordi@hello.nl>
2023-05-22 09:43:31 +0200

    Feature added: gender dropdown

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ def index():
 def greet():
     name = request.form['name']
     age = request.form['age']
-    return render_template('greeting.html', name=name, age=age)
+    city = request.form['city']
+    gender = request.form['gender']
+    return render_template('greeting.html', name=name, age=age, city=city, gender=gender)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
--- test_app.py
+++ test_app.py
@@ -13,13 +13,15 @@ class TestApp(TestCase):
     def test_index_page(self):
         response = self.client.get(url_for('index'))
         self.assert200(response)
-        self.assertIn(b"Enter Your Name and Age", response.data)
+        self.assertIn(b"Enter Your Name, Age, City and Gender", response.data)
 
     def test_greet_page(self):
-        response = self.client.post(url_for('greet'), data={'name': 'John', 'age': 25}, follow_redirects=True)
+        response = self.client.post(url_for('greet'), data={'name': 'John', 'age': 25, 'city': 'Groningen', 'gender': 'male'}, follow_redirects=True)
         self.assert200(response)
         self.assertIn(b"Hello, John!", response.data)
         self.assertIn(b"You are 25 years old.", response.data)
+        self.assertIn(b"You are a male.", response.data)
+        self.assertIn(b"You live in Groningen.", response.data)
 
 if __name__ == '__main__':
     unittest.main()
