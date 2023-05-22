36109352c41c9420d7bf3a5603ac3d14fbad4141
Jordi <Jordi@hello.nl>
2023-05-22 15:07:17 +0200

    Fix: DOB format

--- test_app.py
+++ test_app.py
@@ -16,10 +16,10 @@ class TestApp(TestCase):
         self.assertIn(b"Enter Your Name, Age, City and Gender", response.data)
 
     def test_greet_page(self):
-        response = self.client.post(url_for('greet'), data={'name': 'John', 'age': 25, 'dob': '25-07-1992', 'city': 'Groningen', 'gender': 'male'}, follow_redirects=True)
+        response = self.client.post(url_for('greet'), data={'name': 'John', 'age': 25, 'dob': '26-07-1992', 'city': 'Groningen', 'gender': 'male'}, follow_redirects=True)
         self.assert200(response)
         self.assertIn(b"Hello, John!", response.data)
-        self.assertIn(b"You are 25 years old and you are born on 25-07-1992.", response.data)
+        self.assertIn(b"You are 25 years old and you are born on 26-07-1992.", response.data)
         self.assertIn(b"You are a male.", response.data)
         self.assertIn(b"You live in Groningen.", response.data)
 
